d83f6709ba1cf41b458b005b58e47ed176bff303
Uladzislau Sazanovich <zerogerc@gmail.com>
2018-04-16 18:45:10 +0300

    Fixes for token-level accuracy (#20)

--- scripts/ast/glove_terminals_train.sh
+++ scripts/ast/glove_terminals_train.sh
@@ -4,4 +4,4 @@
 ./build/cooccur -vocab-file vocab.txt -memory 1 < corpus.txt > cooccurrence.bin
 ./build/shuffle -memory 1 < cooccurrence.bin > cooccurrence.shuf.bin
 ./build/glove -input-file cooccurrence.shuf.bin -vocab-file vocab.txt -save-file vectors -verbose 0 \
-    -vector-size 50 -threads 4 -alpha 0.75 -x-max 100.0 -eta 0.05 -binary 2 -model 2 -iter 50
+    -vector-size 100 -threads 4 -alpha 0.75 -x-max 100.0 -eta 0.05 -binary 2 -model 2 -iter 1000 -checkpoint-every 100
