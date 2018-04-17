d4e3669a7aa6a0906b6009db174d3434274a4e24
Uladzislau Sazanovich <zerogerc@gmail.com>
2018-04-17 22:26:47 +0300

    Potential OOM fix

--- scripts/ast/train.sh
+++ scripts/ast/train.sh
@@ -11,17 +11,17 @@ PYTHONPATH=. python3 zerogercrnn/experiments/ast_level/main.py \
     --real_data \
     --cuda \
     --seq_len 50 \
-    --batch_size 100 \
+    --batch_size 80 \
     --learning_rate 0.005 \
     --epochs 20 \
     --decay_after_epoch 0 \
     --decay_multiplier 0.9 \
     --weight_decay=0. \
-    --hidden_size 1500 \
+    --hidden_size 500 \
     --num_layers 1 \
     --dropout 0.01 \
     --non_terminals_num 97 \
-    --non_terminal_embedding_dim 20 \
+    --non_terminal_embedding_dim 10 \
     --terminals_num 50001 \
     --terminal_embedding_dim 100 \
     --terminal_embeddings_file data/ast/terminal_embeddings.txt
