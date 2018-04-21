dff61dcbf12cfda199eacc595bf8b5a9a612169a
Uladzislau Sazanovich <zerogerc@gmail.com>
2018-04-21 22:22:02 +0300

    Great refactor

--- scripts/ast/train.sh
+++ scripts/ast/train.sh
@@ -12,14 +12,14 @@ PYTHONPATH=. python3 zerogercrnn/experiments/ast_level/main.py \
     --cuda \
     --seq_len 50 \
     --batch_size 80 \
-    --learning_rate 0.005 \
+    --learning_rate 0.01 \
     --epochs 20 \
     --decay_after_epoch 0 \
-    --decay_multiplier 0.9 \
+    --decay_multiplier 0.8 \
     --weight_decay=0. \
     --hidden_size 1500 \
     --num_layers 1 \
-    --dropout 0.01 \
+    --dropout 0.03 \
     --non_terminals_num 97 \
     --non_terminal_embedding_dim 10 \
     --terminals_num 50001 \
