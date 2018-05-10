bb4a3033e60a6297b27016f7a656a909b2170eea
Uladzislau Sazanovich <zerogerc@gmail.com>
2018-05-10 15:30:05 +0300

    Remove GloVe from layered model (#44)

--- zerogercrnn/experiments/ast_level/nt2n_layered_attention/main.py
+++ zerogercrnn/experiments/ast_level/nt2n_layered_attention/main.py
@@ -1,16 +1,24 @@
 from zerogercrnn.experiments.ast_level.nt2n_layered_attention.model import NT2NLayeredAttentionModel
-from zerogercrnn.experiments.ast_level.nt2n_te.main import NT2NPretrainedTerminalsMain
+from zerogercrnn.experiments.ast_level.common import ASTMain
+from zerogercrnn.experiments.ast_level.common import NonTerminalMetrics, NonTerminalsCrossEntropyLoss
+from zerogercrnn.lib.metrics import MaxPredictionAccuracyMetrics
 
 
-class NT2NLayeredAttentionMain(NT2NPretrainedTerminalsMain):
+class NT2NLayeredAttentionMain(ASTMain):
 
     def create_model(self, args):
         return NT2NLayeredAttentionModel(
             non_terminals_num=args.non_terminals_num,
             non_terminal_embedding_dim=args.non_terminal_embedding_dim,
-            terminal_embeddings=self.terminal_embeddings,
+            terminals_num=args.terminals_num,
+            terminal_embedding_dim=args.terminal_embedding_dim,
             layered_hidden_size=args.layered_hidden_size,
             hidden_dim=args.hidden_size,
-            prediction_dim=args.non_terminals_num,
             dropout=args.dropout
         )
+
+    def create_criterion(self, args):
+        return NonTerminalsCrossEntropyLoss()
+
+    def create_metrics(self, args):
+        return NonTerminalMetrics(base=MaxPredictionAccuracyMetrics())
