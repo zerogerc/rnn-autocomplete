dc8b101b0c949babd132f5881681dcb4285b4745
Uladzislau Sazanovich <zerogerc@gmail.com>
2018-05-13 14:10:15 +0300

    Comparison and visualization of per non terminal improvement of models

--- zerogercrnn/experiments/ast_level/vis/accuracies.py
+++ zerogercrnn/experiments/ast_level/vis/accuracies.py
@@ -1,6 +1,8 @@
 import numpy as np
 import torch
 
+from zerogercrnn.experiments.ast_level.data import ASTTarget
+from zerogercrnn.experiments.ast_level.metrics import SingleNonTerminalAccuracyMetrics
 from zerogercrnn.lib.metrics import BaseAccuracyMetrics
 
 
@@ -13,13 +15,48 @@ class ResultsReader:
         self.target = np.load(self.results_dir + '/target')
 
 
-if __name__ == '__main__':
-    reader = ResultsReader(results_dir='data/ast/temp')
-    metrics = BaseAccuracyMetrics()
-
+def run_metrics(reader, metrics):
     metrics.drop_state()
     metrics.report((
         torch.from_numpy(reader.predicted),
         torch.from_numpy(reader.target)
     ))
     metrics.get_current_value(should_print=True)
+
+
+def get_accuracy_result(results_dir):
+    reader = ResultsReader(results_dir=results_dir)
+    metrics = BaseAccuracyMetrics()
+
+    run_metrics(reader, metrics)
+
+
+def get_per_nt_result(results_dir, save_dir):
+    reader = ResultsReader(results_dir=results_dir)
+    metrics = SingleNonTerminalAccuracyMetrics(
+        non_terminals_file='data/ast/non_terminals.json',
+        results_dir=save_dir,
+        group=True,
+        dim=None
+    )
+
+    target = ASTTarget(torch.from_numpy(reader.target), None)
+
+    metrics.drop_state()
+    metrics.report((
+        torch.from_numpy(reader.predicted),
+        target
+    ))
+    metrics.get_current_value(should_print=True)
+
+
+if __name__ == '__main__':
+    base_res_dir = 'eval/ast/nt2n_base'
+    base_save_dir = 'eval_local/nt2n_base'
+    layered_attention_res_dir = 'eval/ast/nt2n_layered_attention'
+    layered_attention_save_dir = 'eval_local/nt2n_layered_attention'
+
+    get_per_nt_result(
+        results_dir=base_res_dir,
+        save_dir=base_save_dir
+    )
--- zerogercrnn/experiments/ast_level/vis/compare.py
+++ zerogercrnn/experiments/ast_level/vis/compare.py
@@ -1,7 +1,53 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
-from zerogercrnn.lib.preprocess import read_json
 from zerogercrnn.lib.constants import EOF_TOKEN
+from zerogercrnn.lib.preprocess import read_json, read_jsons
+
+
+def compare_per_nt(file1, file2, y_label):
+    nt1, res1 = list(read_jsons(file1))
+    nt2, res2 = list(read_jsons(file2))
+    assert nt1 == nt2
+
+    x = np.arange(len(nt1))
+    y1 = np.array(res1)
+    y2 = np.array(res2)
+
+    my_xticks = nt1
+    plt.xticks(x, my_xticks, rotation=30, horizontalalignment='right', fontsize=8)
+    plt.ylabel(y_label)
+    plt.grid(True)
+
+    plt.plot(x, (y2 - y1) * 100)
+    plt.show()
+
+    # print('Diff as second - first:')
+    # for i in range(len(nt1)):
+    #     print('{} : {}'.format(nt1[i], res2[i] - res1[i]))
+
+
+def compare_per_two_plots(file1, file2, y_label):
+    nt1, res1 = list(read_jsons(file1))
+    nt2, res2 = list(read_jsons(file2))
+    assert nt1 == nt2
+
+    x = np.arange(len(nt1))
+    y1 = np.array(res1)
+    y2 = np.array(res2)
+
+    my_xticks = nt1
+    plt.xticks(x, my_xticks, rotation=30, horizontalalignment='right', fontsize=8)
+    plt.ylabel(y_label)
+    plt.grid(True)
+
+    plt.plot(x, y1, 'r', x, y2, 'g')
+    plt.show()
+
+    # print('Diff as second - first:')
+    # for i in range(len(nt1)):
+    #     print('{} : {}'.format(nt1[i], res2[i] - res1[i]))
+
 
 def run_main():
     values_base = np.array(read_json('eval/nt2n_base/nt_acc.json'))
@@ -15,4 +61,14 @@ def run_main():
 
 
 if __name__ == '__main__':
-    run_main()
+    # run_main()
+    compare_per_nt(
+        file1='eval_local/nt2n_base/nt_acc.txt',
+        file2='eval_local/nt2n_layered_attention/nt_acc.txt',
+        y_label='Gain of layered attention comparing to base model'
+    )
+    compare_per_two_plots(
+        file1='eval_local/nt2n_base/nt_acc.txt',
+        file2='eval_local/nt2n_layered_attention/nt_acc.txt',
+        y_label='Accuracies: Red - base model, Green - layered model'
+    )
