45bf3ea035560387226efea2e13ab9f7b70a9033
Uladzislau Sazanovich <zerogerc@gmail.com>
2018-05-18 20:37:02 +0300

    Prepare to run

--- scripts/ast/eval.sh
+++ scripts/ast/eval.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
+#!/bin/bash
 
 mkdir saved
 mkdir saved/$2
+mkdir eval
+mkdir eval/$2
+
 PYTHONPATH=. python3 zerogercrnn/experiments/ast_level/main.py \
     --title $2 \
     --prediction $1 \
-    --eval_file "data/ast/file_eval.json" \
-    --data_limit 10000 \
-    --model_save_dir saved/$2 \
-    --saved_model $3 \
-    --cuda \
     --eval \
+    --eval_results_directory eval/$2 \
+    --eval_file "data/ast/file_eval.json" \
+    --saved_model "saved/16May_nt2n_layered_attention_normalized_hs1500_shs500/model_epoch_30" \
+    --data_limit 100 \
+    --model_save_dir saved/$2 \
     --seq_len 50 \
     --batch_size 80 \
-    --learning_rate 0.005 \
-    --epochs 20 \
+    --learning_rate 0.001 \
+    --epochs 30 \
     --decay_after_epoch 0 \
     --decay_multiplier 0.9 \
     --weight_decay=0. \
-    --hidden_size 1000 \
+    --hidden_size 1500 \
     --num_layers 1 \
     --dropout 0.01 \
+    --layered_hidden_size 500 \
     --non_terminals_num 97 \
-    --non_terminal_embedding_dim 10 \
+    --non_terminal_embedding_dim 20 \
+    --non_terminals_file "data/ast/non_terminals.json" \
+    --non_terminal_embeddings_file "data/ast/non_terminal_embeddings.txt" \
     --terminals_num 50001 \
     --terminal_embedding_dim 100 \
-    --terminal_embeddings_file data/ast/terminal_embeddings.txt
+    --terminals_file "data/ast/terminals.json" \
+    --terminal_embeddings_file "data/ast/terminal_embeddings.txt" \
+    --nodes_depths_stat_file "eval/ast/stat/node_depths.json"
 
--- scripts/ast/run.sh
+++ scripts/ast/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./scripts/ast/train.sh nt2n_layered_attention 16May_nt2n_layered_attention_normalized_hs1500_shs500
+./scripts/ast/eval.sh nt2n_layered_attention 16May_nt2n_layered_attention_normalized_hs1500_shs500_eval
