cda1a28a43a7f79d3bf127dab3df183eede861f6
Uladzislau Sazanovich <zerogerc@gmail.com>
2018-05-20 14:32:43 +0300

    Prepare normalized model to run

--- scripts/ast/run.sh
+++ scripts/ast/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./scripts/ast/train.sh nt2n_layered_attention_norm 19May_nt2n_layered_attention_fully_normalized_hs1500_shs500
+./scripts/ast/train.sh nt2n_layered_attention_norm 20May_nt2n_layered_attention_eq_emb_50_hs1500_shs500
