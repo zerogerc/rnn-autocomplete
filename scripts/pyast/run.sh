c7a6b8ca206d02bc940119f9d6f9b1588ea48b17
Uladzislau Sazanovich <zerogerc@gmail.com>
2018-06-03 14:01:01 +0300

    python run fix 2

--- scripts/pyast/run.sh
+++ scripts/pyast/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./scripts/pyast/data_download.sh
-./sripts/pyast/data_process.sh
+./scripts/pyast/data_process.sh
 ./scripts/pyast/train.sh nt2n_base_attention_plus_layered 03Jun_py_nt2n_base_attention_plus_layered_hs1500_lhs500
