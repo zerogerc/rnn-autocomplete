9fb5e59db7fd7b3dd77a9052b2aa3f36f6aaa457
Uladzislau Sazanovich <zerogerc@gmail.com>
2018-06-03 13:57:16 +0300

    Python run

--- scripts/pyast/data_download.sh
+++ scripts/pyast/data_download.sh
@@ -3,7 +3,7 @@
 wget 'http://files.srl.inf.ethz.ch/data/py150.tar.gz'
 gunzip -c py150.tar.gz | tar xopf -
 rm py150.tar.gz
-mkdir pyast
-mv python50k_eval.json pyast/python50k_eval.json
-mv python100k_train.json pyast/python100k_train.json
+mkdir data/pyast
+mv python50k_eval.json data/pyast/python50k_eval.json
+mv python100k_train.json data/pyast/python100k_train.json
 cd ..
