0448f3f85c0b0fb150a080259137920c6cb22585
Uladzislau Sazanovich <Uladzislau.Sazanovich@jetbrains.com>
2019-05-19 16:50:16 +0300

    fixes

--- scripts/ast/run.sh
+++ scripts/ast/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./scripts/ast/eval.sh nt2n_base_attention_plus_layered eval_nt2n_base_attention_plus_layered_large_embeddings
+./scripts/ast/train.sh nt2n_base_attention_plus_layered eval_nt2n_base_attention_plus_layered_large_embeddings
--- scripts/ast/train.sh
+++ scripts/ast/train.sh
@@ -12,7 +12,6 @@ PYTHONPATH=. python3 zerogercrnn/experiments/ast_level/main.py \
     --train_file "data/ast/file_train.json" \
     --data_limit 100000 \
     --model_save_dir saved/$2 \
-    --saved_model "saved/04Jun_nt2n_base_attention_plus_layered_new_embedding_size/model_epoch_3" \
     --seq_len 50 \
     --batch_size 128 \
     --learning_rate 0.0001 \
